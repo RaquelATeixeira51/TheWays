49d6d858cc2279dfb2b9a5f7d02cfbe266e611d0
RAQUEL APARECIDA TEIXEIRA <raquel.ateixeira3@senacsp.edu.br>
2022-05-23 21:50:56 -0300

    atualicoes hugo

--- src/main/java/com/mycompany/theways/Main.java
+++ src/main/java/com/mycompany/theways/Main.java
@@ -238,7 +238,7 @@ public class Main {
         public static void Final() {
             Estrutura.cabecalho("THE END");
             System.out.println(
-                    "Thomas se encontra em seu laboratório novamente, porém por conta de todo ocorrido dos últimos tempos, sua memória acabou se fragmentando fazendo-o esquecer de algumas coisas.\nHugo adentra seu local de trabalho feliz por finalmente ter reencontrado seu avô, e emocionado pergunta sobre sua mãe adotiva “Vô, onde está mamãe?”\nThomas um pouco confuso responde “Sua mãe não está mais entre nós a um tempo…”\nE Hugo rebate:  “Estou falando a Alex…”\nE por fim Thomas devolve a pergunta: “Quem é Alex?”");
+                    "Thomas se encontra em seu laboratório novamente, porém, por conta de todo ocorrido dos últimos tempos, sua memória acabou se fragmentando fazendo-o esquecer de algumas coisas.\nHugo adentra seu local de trabalho feliz por finalmente ter reencontrado seu avô, e emocionado pergunta sobre sua mãe adotiva “Vô, onde está mamãe?”\nThomas um pouco confuso responde “Sua mãe não está mais entre nós a um tempo…”\nE Hugo rebate:  “Estou falando a Alex…”\nE por fim Thomas devolve a pergunta: “Quem é Alex?”");
         }
     }
 
@@ -280,15 +280,81 @@ public class Main {
 
         public static void SegundoAto() {
             Estrutura.cabecalho("SEGUNDO ATO");
-            System.out.println("Ao se esconder embaixo da mesa, Hugo ouve sobre o ocorrido de dias atrás com seu bisavô e sua tia.");
+            System.out.println("Ao me esconder embaixo da mesa, ouço sobre o ocorrido de dias atrás com meu bisavô e minha tia.\n\n Agente 1:  “Nunca vi nada desse tipo…” \n\n Agente 2 : “Todos os radares de energia dispararam!” \n\n Agente 3: “Ficaram sabendo? Um dos procurados foi para o futuro e outro para o passado.” \n\n");
+            System.out.println("Chefe do FBI: “Agentes, isso não pode sair deste laboratório. Os agentes do tempo já estão atrás dos responsáveis. Vamos aguardar e ver se achamos alguma evidência do que ocorreu aqui.”");
             Estrutura.qualquerCoisaParaContinuar();
-            TerceiroAto();
+            System.out.println("Ao perceber que os agentes irão vasculhar o local, preciso rapidamente pensar em como fugir sem ser pego.");
+            boolean jogoSegue=false;
+
+            while(!jogoSegue){
+                System.out.println("O que devo fazer?");
+                System.out.println("[1] Sair correndo e tentar a sorte de não ser pego");
+                System.out.println("[2] Tentar roubar a arma do agente.");
+                System.out.println("[3] Engatinhar até uma saída secreta que levava até o esgoto.");
+
+                int input = Estrutura.leiaInput("->", 3);
+
+                switch(input){
+                    case 1:
+                    System.out.println("Infelizmente você não era rápido como pensava, haviam muitos agentes e conseguiram te capturar.\nGAME OVER.");
+                    case 2:
+                    System.out.println("O tiro saiu pela culatra! Hugo não foi sorrateiro o suficiente ao pegar a arma do agente, e em um momento de susto o agente saca a arma e atira nele.\nGAME OVER.");
+                    break;
+                    case 3:
+                    TerceiroAto();
+                    jogoSegue=true;
+                }
+            }
         }
 
         public static void TerceiroAto() {
             Estrutura.cabecalho("TERCEIRO ATO");
+            System.out.println("Consegui sair com sucesso pelo esgoto, mas durante minha caminhada procurando a saída do prédio penso na frase que um dos agentes falou sobre um de meus familiares ter ido pro passado.\nMinha característica é ser esperto apesar da pouca idade e em uma tentativa de salvar minha família, penso em procurar alguma pista em um livro de história.");
             Estrutura.qualquerCoisaParaContinuar();
-            Historia.Final();
+            System.out.println("Dias se passaram, eu me encontrava deitado no chão rodeado de livros e latinhas de energético.\nE como última tentativa, vejo na prateleira de minha casa 3 últimos livros. ");
+            Estrutura.qualquerCoisaParaContinuar();
+            boolean jogoSegue=false;
+
+            while(!jogoSegue){
+                System.out.println("O que devo fazer?");
+                System.out.println("[1] Pego o livro sobre a Segunda Guerra Mundial.");
+                System.out.println("[2] Pego o livro sobre Idade Média.");
+                System.out.println("[3] Pego o livro sobre mulheres que fizeram história.");
+
+                int input = Estrutura.leiaInput("->", 3);
+
+                switch(input){
+                    case 1:
+                    System.out.println("Apesar de querer encontrar muito minha tia, eu não sabia se ficava aliviado ou desesperado por não conter nada sobre ela no livro. Infelizmente continuo sem pistas e desisto da busca.\nGAME OVER.");
+                    case 2:
+                    jogoSegue=true; 
+                    case 3:
+                    System.out.println("Apesar de minha tia ter o perfil para se encaixar naquele livro com facilidade, não havia nada sobre ela. Continuo sem pistas e cansado acabo desistindo da procura.\nGAME OVER.");
+                }
+            }
+           
+            System.out.println("Apesar da pista encontrada, o final do livro não continha o destino daquela mulher, então decido voltar ao laboratório e procurar pistas sobre minha mãe adotiva.\nAo chegar na rua do laboratório, percebo que a entrada pela qual entrei da primeira vez estava repleta de agentes.\n\nEnquanto pensava no que fazer, percebo alguém em minha frente me puxando pelo pescoço");
+            boolean jogoSegue2=false;
+
+            while(!jogoSegue2){
+                System.out.println("O que devo fazer?");
+                System.out.println("[1] Tento acertar a cara do agente.");
+                System.out.println("[2] Tento chutar o saco do agente.");
+                System.out.println("[3] Tento afrouxar os dedos do agente que estavam em meu pescoço.");
+               
+                int input = Estrutura.leiaInput("->", 3);
+
+                switch(input){
+                    case 1:
+                    System.out.println("Acerto o rosto do agente porém não forte o suficiente para fazer ele me soltar.\nGAME OVER.");
+                    case 2:
+                    System.out.println("Assim que consigo me desvencilhar do agente, corri para o bueiro pelo qual saíra da última vez.\nEnquanto caminho pelos esgotos, me questiono sobre o que poderia encontrar.\nAlgo estaria diferente? Estaria tudo igual?");
+                    jogoSegue2=true;
+                    Historia.Final();
+                    case 3:
+                    System.out.println("Me debato e tento soltar os dedos do agente que estavam em meu pescoço, mas ele era muito forte.\nGAME OVER.");
+                }
+            }
         }
     }
 
